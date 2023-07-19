763ad8747cc1f5cc420eddd30566f644eb41bef3
Shivansh <shivanshkumar760@gmail.com>
2023-07-19 21:28:08 +0530

    Sixteen Commit

--- Facts/02.JS_Day3/WebSite_JS/09.LoopinAndMap/script.js
+++ Facts/02.JS_Day3/WebSite_JS/09.LoopinAndMap/script.js
@@ -239,3 +239,79 @@ console.log(arr2);
 // We can use arrow function also to define the function in map
 const arr3=arr1.map((i)=>i*4);
 console.log(arr3);
+// So now we have studied about function,array,object and map lets make use of it 
+
+const CATEGORIES = [
+    { name: "technology", color: "#3b82f6" },
+    { name: "science", color: "#16a34a" },
+    { name: "finance", color: "#ef4444" },
+    { name: "society", color: "#eab308" },
+    { name: "entertainment", color: "#db2777" },
+    { name: "health", color: "#14b8a6" },
+    { name: "history", color: "#f97316" },
+    { name: "news", color: "#8b5cf6" },
+  ];
+
+// So here we have array of object 
+// Now we want to create another array of only category name key in the array of object so we will use 
+//function and map for that
+
+const allCategories = CATEGORIES.map((el)=>el.name);
+console.log(allCategories);
+const initialFacts = [
+    {
+      id: 1,
+      text: "React is being developed by Meta (formerly facebook)",
+      source: "https://opensource.fb.com/",
+      category: "technology",
+      votesInteresting: 24,
+      votesMindblowing: 9,
+      votesFalse: 4,
+      createdIn: 2021,
+    },
+    {
+      id: 2,
+      text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+      source:
+        "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+      category: "society",
+      votesInteresting: 11,
+      votesMindblowing: 2,
+      votesFalse: 0,
+      createdIn: 2019,
+    },
+    {
+      id: 3,
+      text: "Lisbon is the capital of Portugal",
+      source: "https://en.wikipedia.org/wiki/Lisbon",
+      category: "society",
+      votesInteresting: 8,
+      votesMindblowing: 3,
+      votesFalse: 1,
+      createdIn: 2015,
+    },
+  ];
+
+//   So now we can see there is a createdIn key with the year in which this fact was posted 
+//so now we want to find the age of the fact via using the year createdIn  
+//We will use map for it 
+function calcFactAge(year)
+{
+    
+    const currentYear = new Date().getFullYear();
+    const age= currentYear - year;
+    if(age>0)
+    {
+        return age;
+    }
+    else
+    {
+        // return "Impossible year";
+        return `Impossible year.Year needs to be less or equal to ${currentYear}`;
+    }
+}
+
+const ageFact=initialFacts.map((el)=>calcFactAge(el.createdIn));
+console.log(ageFact);
+
+//This was all about forEach and map Method 

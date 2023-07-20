3902700cb54c36fa5c08b95d34ec1487dc52f632
Shivansh <shivanshkumar760@gmail.com>
2023-07-20 19:15:38 +0530

    Twenty Commit

---
+++ Facts/02.JS_Day3/WebSite_JS/13.FilterAndFindArrayMethod/filter.js
@@ -0,0 +1,10 @@
+// So filter is a method associated with array which basically means to filter out the element 
+//which satisfy the function/condition
+
+const exArray=[7,23,8,10,43];
+const filterArray=exArray.filter((el)=>el>10);
+console.log(exArray);
+console.log(filterArray);
+
+
+
---
+++ Facts/02.JS_Day3/WebSite_JS/13.FilterAndFindArrayMethod/find.js
@@ -0,0 +1,6 @@
+// So find is just like filter but instead of returning a array it return a single value 
+//which comes first in array
+const exArray=[7,53,8,10,43];
+const filterArray=exArray.find((el)=>el>10);
+console.log(exArray);
+console.log(filterArray);
